4ed2af6f136887314d83d7ff72b3888c9fb22e93
kolirina <149939917+kolirina@users.noreply.github.com>
2024-03-19 17:12:24 +0300

    feat: RSS-PZ-15_Implement_Drag-and-Drop (#51)

--- rs-puzzle/src/app/gamePage/gamePage.ts
+++ rs-puzzle/src/app/gamePage/gamePage.ts
@@ -1,5 +1,6 @@
-import { Round, GameData, WordCard } from "../interfaces";
-import fetchRoundsData from "../fetchData";
+import { Round, GameData, WordCard } from '../interfaces';
+import fetchRoundsData from '../fetchData';
+import LoginForm from '../loginPage/loginForm';
 
 export default class GamePage {
   private gameData: GameData;
@@ -7,60 +8,59 @@ export default class GamePage {
   private currentRoundIndex: number = 0;
   private textExampleIndex: number = 0;
   private newContainers: HTMLDivElement[] = [];
+  private gameContainer!: HTMLDivElement;
   private container!: HTMLDivElement;
-  private continueButton: HTMLButtonElement = document.createElement("button");
+  private draggedElement: HTMLElement | null = null;
+  private continueButton: HTMLButtonElement = document.createElement('button');
   private words: string[] = [];
   private originalOrder: string[] = [];
 
   constructor() {
     this.gameData = { rounds: [], roundsCount: 0 };
     this.initGame();
-    this.continueButton.addEventListener(
-      "click",
-      this.handleContinue.bind(this),
-    );
+    this.continueButton.addEventListener('click', this.handleContinue.bind(this));
   }
 
   public async initGame(): Promise<void> {
     try {
       this.gameData = await fetchRoundsData();
       this.rounds = this.gameData.rounds;
+      const gameContainer = document.createElement('div');
+      gameContainer.id = 'game-container';
+      gameContainer.classList.add('game-container');
+      document.body.appendChild(gameContainer);
       if (this.rounds.length > 0) {
-        this.container = document.createElement("div");
-        this.container.id = "word-container";
-        document.body.appendChild(this.container);
+        this.container = document.createElement('div');
+        this.container.id = 'word-container';
+        gameContainer.appendChild(this.container);
         this.startGame();
       }
-      console.log("запущена initgame");
-      const paintingContainer = document.createElement("div");
-      paintingContainer.id = "painting-container";
-      paintingContainer.classList.add("painting-container");
-      document.body.appendChild(paintingContainer);
+      console.log('запущена initgame');
+      const paintingContainer = document.createElement('div');
+      paintingContainer.id = 'painting-container';
+      paintingContainer.classList.add('painting-container');
+      gameContainer.appendChild(paintingContainer);
 
       for (let i = 0; i < 10; i++) {
-        const newContainer = document.createElement("div");
+        const newContainer = document.createElement('div');
         newContainer.id = `new-container-${i}`;
-        newContainer.classList.add("new-container");
+        newContainer.classList.add('new-container');
         this.newContainers.push(newContainer);
         paintingContainer.appendChild(newContainer);
-        newContainer.addEventListener(
-          "dragover",
-          this.handleDragOver.bind(this),
-        );
-        newContainer.addEventListener("drop", this.handleDrop.bind(this));
+        newContainer.addEventListener('dragover', this.handleDragOver.bind(this));
+        newContainer.addEventListener('drop', this.handleDrop.bind(this));
       }
     } catch (error) {
-      console.error("Error initializing game:", error);
+      console.error('Error initializing game:', error);
     }
   }
 
   public startGame(): void {
-    console.log("запущена startgame");
     const textExamplesByLevel = this.getTextExamplesByLevel();
     const currentRound = this.rounds[this.currentRoundIndex];
     const currentLevelId = currentRound.levelData.id;
     const textExamples = textExamplesByLevel[currentLevelId];
-    this.continueButton.removeEventListener("click", this.handleContinue);
+    this.continueButton.removeEventListener('click', this.handleContinue);
 
     if (textExamples && textExamples.length > 0) {
       const sentence = textExamples[this.textExampleIndex];
@@ -70,35 +70,29 @@ export default class GamePage {
       this.renderWordContainer(shuffledWords);
       this.renderContinueButton();
     } else {
-      console.error("No text examples found for the current level.");
+      console.error('No text examples found for the current level.');
     }
   }
 
   private renderContinueButton(): void {
-    console.log("внутри renderContinueButton");
-    this.continueButton.textContent = "Continue";
+    console.log('внутри renderContinueButton');
+    this.continueButton.textContent = 'Continue';
+    this.continueButton.classList.add('continue-button');
     this.continueButton.disabled = true;
     document.body.appendChild(this.continueButton);
   }
 
   private handleContinue(): void {
-    if (
-      this.textExampleIndex <
-      this.rounds[this.currentRoundIndex].words.length - 1
-    ) {
-      console.log("запущен handlecontinue");
+    if (this.textExampleIndex < this.rounds[this.currentRoundIndex].words.length - 1) {
+      console.log('запущен handlecontinue');
       this.textExampleIndex++;
       console.log(this.textExampleIndex);
-      this.container.innerHTML = "";
+      this.container.innerHTML = '';
       this.startGame();
     } else {
-      console.log("End of text examples reached for the current round.");
-      this.continueButton.removeEventListener(
-        "click",
-        this.handleContinue.bind(this),
-      );
+      console.log('End of text examples reached for the current round.');
+      this.continueButton.removeEventListener('click', this.handleContinue.bind(this));
     }
-
   }
 
   private shuffleArray<T>(array: T[]): T[] {
@@ -111,32 +105,28 @@ export default class GamePage {
 
   private renderWordContainer(words: string[]): void {
     words.forEach((word) => {
-      console.log("внутри renderWordContainer");
-      const wordElement = document.createElement("div");
+      console.log('внутри renderWordContainer');
+      const wordElement = document.createElement('div');
       wordElement.textContent = word;
-      wordElement.classList.add("word-element");
+      wordElement.classList.add('word-element');
       wordElement.draggable = true;
       const wordWidth = Math.max(50, word.length * 10);
-      wordElement.style.width = wordWidth + "px";
-      wordElement.addEventListener(
-        "click",
-        this.handleWordCardClick.bind(this),
-      );
+      wordElement.style.width = wordWidth + 'px';
+      wordElement.addEventListener('click', this.handleWordCardClick.bind(this));
       this.container.appendChild(wordElement);
 
-      wordElement.addEventListener(
-        "dragstart",
-        this.handleDragStart.bind(this),
-      );
-      wordElement.addEventListener("dragover", this.handleDragOver.bind(this));
-      wordElement.addEventListener("drop", this.handleDrop.bind(this));
+      wordElement.addEventListener('dragstart', this.handleDragStart.bind(this));
+      wordElement.addEventListener('dragover', this.handleDragOver.bind(this));
+      wordElement.addEventListener('drop', this.handleDrop.bind(this));
     });
   }
 
   private handleDragStart(event: DragEvent): void {
-    const draggedElement = event.target as HTMLElement;
-    draggedElement.classList.add("dragged");
-    event.dataTransfer?.setData("text/plain", "dragged");
+    this.draggedElement = event.target as HTMLElement;
+    if (this.draggedElement) {
+      this.draggedElement.classList.add('dragged');
+      event.dataTransfer?.setData('text/plain', 'dragged');
+    }
   }
 
   private handleDragOver(event: DragEvent): void {
@@ -145,85 +135,88 @@ export default class GamePage {
 
   private handleDrop(event: DragEvent): void {
     event.preventDefault();
-    const draggedElement = document.querySelector(".dragged");
-    if (draggedElement) {
+
+    // Проверяем, что this.draggedElement не равен null и содержит один из классов
+    if (
+      this.draggedElement &&
+      (this.draggedElement.classList.contains('word-element') ||
+        this.draggedElement.classList.contains('new-container-element'))
+    ) {
       const targetElement = event.target as HTMLElement;
-      const targetContainerIndex = this.newContainers.findIndex(
-        (container) => container.id === targetElement.id,
-      );
-      if (targetContainerIndex !== -1) {
-        const newDiv = document.createElement("div");
-        newDiv.textContent = draggedElement.textContent;
-        newDiv.classList.add("new-container-element");
-        this.newContainers[targetContainerIndex].appendChild(newDiv);
-        if (draggedElement.textContent) {
-          const wordWidth = Math.max(
-            50,
-            draggedElement.textContent.length * 10,
-          );
-          newDiv.style.width = wordWidth + "px";
+
+      // Проверяем, куда был брошен элемент
+      if (targetElement.classList.contains('new-container')) {
+        const targetContainerIndex = this.newContainers.findIndex((container) => container.id === targetElement.id);
+        if (targetContainerIndex !== -1) {
+          const newDiv = document.createElement('div');
+          newDiv.textContent = this.draggedElement.textContent;
+          newDiv.classList.add('new-container-element');
+          this.newContainers[targetContainerIndex].appendChild(newDiv);
+          if (this.draggedElement.textContent) {
+            const wordWidth = Math.max(50, this.draggedElement.textContent.length * 10);
+            newDiv.style.width = wordWidth + 'px';
+            newDiv.addEventListener('click', this.handleWordCardClick.bind(this));
+          }
+          this.draggedElement.remove();
+          this.checkOrder();
         }
-        draggedElement.remove();
+      } else {
+        const wordElement = document.createElement('div');
+        wordElement.textContent = this.draggedElement.textContent;
+        wordElement.classList.add('word-element');
+        this.container.appendChild(wordElement);
+        if (this.draggedElement.textContent) {
+          const wordWidth = Math.max(50, this.draggedElement.textContent.length * 10);
+          wordElement.style.width = wordWidth + 'px';
+          wordElement.addEventListener('click', this.handleWordCardClick.bind(this));
+        }
+        this.draggedElement.remove();
         this.checkOrder();
       }
+    } else {
+      console.error('draggedElement is null');
     }
   }
   private handleWordCardClick(event: MouseEvent): void {
     const clickedCard = event.target as HTMLElement;
-    if (!clickedCard.classList.contains("word-element")) return;
+    if (clickedCard.classList.contains('word-element') || clickedCard.classList.contains('new-container-element')) {
+      const sourceContainer = clickedCard.parentElement as HTMLElement;
+      const targetContainer =
+        sourceContainer === this.container ? this.newContainers[this.textExampleIndex] : this.container;
 
-    const clickedWord = clickedCard.textContent?.trim();
-    if (!clickedWord) return;
+      targetContainer.appendChild(clickedCard);
 
-    const sourceContainer = this.container;
-
-    const resultContainer = this.newContainers[this.textExampleIndex];
-
-    if (sourceContainer && clickedCard.parentElement === sourceContainer) {
-      if (resultContainer) {
-        clickedCard.classList.add("new-container-element");
-        resultContainer.appendChild(clickedCard);
-      }
-    } else if (
-      resultContainer &&
-      clickedCard.parentElement === resultContainer
-    ) {
-      if (sourceContainer) {
-        sourceContainer.appendChild(clickedCard);
-        clickedCard.classList.remove("new-container-element");
-      }
+      clickedCard.classList.toggle('new-container-element');
+      clickedCard.classList.toggle('word-element');
+      clickedCard.addEventListener('dragstart', this.handleDragStart.bind(this));
+      clickedCard.addEventListener('dragover', this.handleDragOver.bind(this));
+      clickedCard.addEventListener('drop', this.handleDrop.bind(this));
     }
-
     this.checkOrder();
   }
 
   private checkOrder(): void {
     const resultContainerElements = Array.from(
-      this.newContainers[this.textExampleIndex].querySelectorAll(
-        ".new-container-element",
-      ),
+      this.newContainers[this.textExampleIndex].querySelectorAll('.new-container-element')
     );
-    const wordsInContainer = resultContainerElements.map(
-      (element) => element.textContent?.trim() || "",
-    );
+    const wordsInContainer = resultContainerElements.map((element) => element.textContent?.trim() || '');
 
     for (let i = 0; i < this.words.length; i++) {
       if (this.originalOrder[i] !== wordsInContainer[i]) {
-        console.log("Does not match");
+        console.log('Does not match');
         return;
       }
     }
     this.continueButton.disabled = false;
-    console.log("🎉");
+    console.log('🎉');
+
   }
 
   private getTextExamplesForLevels(): Record<string, string[]> {
     const textExamplesByLevel: Record<string, string[]> = {};
     this.rounds.forEach((round) => {
       const levelId = round.levelData.id;
-      const textExamples: string[] = round.words.map(
-        (word: WordCard) => word.textExample,
-      );
+      const textExamples: string[] = round.words.map((word: WordCard) => word.textExample);
       textExamplesByLevel[levelId] = textExamples;
     });
     console.log(textExamplesByLevel);
@@ -232,6 +225,5 @@ export default class GamePage {
 
   public getTextExamplesByLevel(): Record<string, string[]> {
     return this.getTextExamplesForLevels();
-
   }
 }
--- rs-puzzle/src/index.ts
+++ rs-puzzle/src/index.ts
@@ -1,17 +1,15 @@
-import LoginForm from "./app/loginPage/loginForm";
-import GamePage from "./app/gamePage/gamePage";
-import "./app/loginPage/loginPage.css";
-import "./app/welcomePage.css";
-import "./app/gamePage/gamePage.css";
-import fetchRoundsData from "./app/fetchData";
-import { IUser, Round, GameData } from "./app/interfaces";
+import LoginForm from './app/loginPage/loginForm';
+import GamePage from './app/gamePage/gamePage';
+import './app/loginPage/loginPage.css';
+import './app/welcomePage.css';
+import './app/gamePage/gamePage.css';
+import fetchRoundsData from './app/fetchData';
+import { IUser, Round, GameData } from './app/interfaces';
 
 fetchRoundsData()
   .then((gameData) => {
     const loginForm = new LoginForm([gameData]);
   })
   .catch((error) => {
-    console.error("Error loading rounds data:", error);
+    console.error('Error loading rounds data:', error);
   });
-
-
